483a9adcc79935bd7dfd986e73612d193c928586
leonardolim78 <leonardolim178@gmail.com>
2022-04-17 22:55:50 +0700

    Add icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ const Document = () => {
     return (
         <Html lang="en">
             <Head>
+                <link rel="icon" href="/favicon.ico" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
