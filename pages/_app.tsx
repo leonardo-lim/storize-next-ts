fa6039d4f4f50eba7446b9a226d10458217694e7
leonardolim78 <leonardolim178@gmail.com>
2022-04-30 11:59:12 +0700

    Import bootstrap in root page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import Layout from '../components/Layout';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+    useEffect(() => {
+        import('bootstrap/dist/js/bootstrap.bundle.min'!);
+    }, []);
+
     return (
         <Layout>
             <Component {...pageProps} />
