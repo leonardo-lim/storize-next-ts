a849e374f8cd2d898558a2277cb16c8c32ecc46b
leonardolim78 <leonardolim178@gmail.com>
2022-05-07 14:31:10 +0700

    Fix import directory path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
-import Layout from '../components/Layout';
+import Layout from '../components/Layout/Layout';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     useEffect(() => {
