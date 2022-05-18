77ffa5ec3e6fe01236056646559f0a40b8dd9f33
leonardolim78 <leonardolim178@gmail.com>
2022-05-18 14:47:22 +0700

    Add order summary component

---
+++ components/Checkout/OrderSummary.tsx
@@ -0,0 +1,111 @@
+import Link from 'next/link';
+import { useContext, useEffect, useState } from 'react';
+import { AmountContext } from '../Layout/Layout';
+
+interface ItemDataType {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: {
+        rate: number;
+        count: number;
+    };
+    quantity: number;
+    unitPrice: number;
+    quantityError: boolean;
+}
+
+interface OrderSummaryProps {
+    feeIndex: number;
+}
+
+const OrderSummary: React.FC<OrderSummaryProps> = ({ feeIndex }) => {
+    const [subtotalPrice, setSubtotalPrice] = useState<number>(0);
+    const [taxPrice, setTaxPrice] = useState<number>(0);
+    const [shippingPrice, setShippingPrice] = useState<number>(0);
+    const [totalPrice, setTotalPrice] = useState<number>(0);
+
+    const { setAmount } = useContext(AmountContext)!;
+
+    const completeOrder = () => {
+        setAmount(0);
+        localStorage.setItem('items', JSON.stringify([]));
+    };
+
+    useEffect(() => {
+        const rawData = localStorage.getItem('items');
+
+        if (rawData) {
+            const data: ItemDataType[] = JSON.parse(rawData);
+
+            const newSubtotalPrice = data.reduce((total, current) => {
+                return total + current.price;
+            }, 0);
+
+            const newTaxPrice = newSubtotalPrice / 10;
+            let newShippingPrice;
+
+            if (feeIndex === 0) {
+                newShippingPrice = 0;
+            } else if (feeIndex === 1) {
+                newShippingPrice = 1;
+            } else {
+                newShippingPrice = 5;
+            }
+
+            const newTotalPrice = newSubtotalPrice + newTaxPrice + newShippingPrice;
+
+            setSubtotalPrice(parseFloat(newSubtotalPrice.toFixed(2)));
+            setTaxPrice(parseFloat(newTaxPrice.toFixed(2)));
+            setShippingPrice(parseFloat(newShippingPrice.toFixed(2)));
+            setTotalPrice(parseFloat(newTotalPrice.toFixed(2)));
+        }
+    }, [feeIndex]);
+
+    return (
+        <>
+            <h3>Order Summary</h3>
+            <hr />
+            <div className="row mb-2">
+                <div className="col-6 text-start">
+                    <h6>Subtotal price</h6>
+                </div>
+                <div className="col-6 text-end">
+                    <h6>${subtotalPrice}</h6>
+                </div>
+            </div>
+            <div className="row mb-2">
+                <div className="col-6 text-start">
+                    <h6>Tax price</h6>
+                </div>
+                <div className="col-6 text-end">
+                    <h6>${taxPrice}</h6>
+                </div>
+            </div>
+            <div className="row mb-2">
+                <div className="col-6 text-start">
+                    <h6>Shipping price</h6>
+                </div>
+                <div className="col-6 text-end">
+                    <h6>${shippingPrice}</h6>
+                </div>
+            </div>
+            <div className="row mb-2">
+                <div className="col-6 text-start">
+                    <h6 className="fw-bold">Total price</h6>
+                </div>
+                <div className="col-6 text-end">
+                    <h6 className="fw-bold">${totalPrice}</h6>
+                </div>
+            </div>
+            <Link href="/completed">
+                <a className="btn btn-gold w-100" onClick={completeOrder}>Complete Order</a>
+            </Link>
+        </>
+    );
+};
+
+export default OrderSummary;
