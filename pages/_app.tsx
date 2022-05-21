138975037fd2c65790689fb5cb801f81488609ec
leonardolim78 <leonardolim178@gmail.com>
2022-05-21 22:14:06 +0700

    Disable react dev tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,20 @@ import { useEffect } from 'react';
 import Layout from '../components/Layout/Layout';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+    const disableReactDevTools = () => {
+        const noop = () => undefined;
+        const DEV_TOOLS = (window as unknown as Record<string, Record<string, unknown>>).__REACT_DEVTOOLS_GLOBAL_HOOK__;
+
+        if (typeof DEV_TOOLS === 'object') {
+            for (const [key, value] of Object.entries(DEV_TOOLS)) {
+                DEV_TOOLS[key] = typeof value === 'function' ? noop : null;
+            }
+        }
+    };
+
     useEffect(() => {
         import('bootstrap/dist/js/bootstrap.bundle.min'!);
+        disableReactDevTools();
     }, []);
 
     return (
